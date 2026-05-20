78641b6a4d01b8920c025de681fc2bf0e9392533
Sapana Chandanshive <sapanasonavane12@gmail.com>
2026-05-20 10:26:18 +0000

    version updated

--- cicd-app.py
+++ cicd-app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps CI/CD vergion 2 Running 🚀"
+    return "Hello use poll scm 2 Running 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
